3dd14c28cc32d7b56eb887af01abd5d82076e44a
flavio.destefano <flavio.destefano@caffeina.com>
2018-01-26 19:51:01 +0100

    FC

---
+++ README.md
@@ -0,0 +1,10 @@
+# CSV-API Server
+
+Simple API Server that uses SQLite for storage, CSV for data input
+
+### Features
+
+* CSV Upload
+* SQLite storage
+* Column filtering
+* Pagination
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+define('START', microtime(1));
+header("Content-Type: application/json");
+
+try {
+	$db = new PDO('sqlite:data.sqlite3');
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->exec("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, title TEXT, message TEXT, visible INTEGER)");
+
+	switch ($_SERVER['REQUEST_METHOD']) {
+		case 'POST':
+		$fh = @fopen($_FILES['csv']['tmp_name'], 'r+');
+		if ($fh == false) throw new Exception('Unable to open file'); 
+		$stmt = $db->prepare("INSERT INTO data (id, title, message, visible) VALUES (:id, :title, :message, :visible)");
+		$skip = true;
+		$rows = 0;
+		while(($row = fgetcsv($fh, 8192)) !== false) {
+			if ($skip === true) {
+				$db->query('DELETE FROM data');
+				$skip = false;
+			} else {
+				$rows++;
+				$stmt->execute($row);
+			}
+		}
+		$out = ([ 'success' => true, 'rows' => $rows ]);
+		break;
+		case 'GET':
+		if (isset($_GET['id'])) {	
+			$stmt = $db->prepare("SELECT * FROM data WHERE id = :id AND visible = 1");
+			$stmt->execute([ $_GET['id'] ]);
+			$out = ['data' => $stmt->fetch(PDO::FETCH_OBJ)];
+		} else { 
+			$limit = intval(isset($_GET['limit']) ? $_GET['limit'] : 20);
+			$page = intval(isset($_GET['page']) ? $_GET['page'] : 1);
+			$sqlpage = ($page-1) * $limit;
+			$stmt = $db->prepare("SELECT * FROM data WHERE visible = 1 LIMIT $sqlpage, $limit");
+			$stmt->execute();
+			$out = ['data' => $stmt->fetchAll(PDO::FETCH_OBJ), 'page' => $page, 'limit' => $limit];
+		}
+		break;
+	}
+} catch (Exception $e) {
+	$out = ['error' => [ 'message' => $e->getMessage() ]];
+}
+
+$out['benchmark'] = microtime(1) - START;
+echo json_encode($out, JSON_NUMERIC_CHECK);
